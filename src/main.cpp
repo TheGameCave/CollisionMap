738e0441a78c84be16d56a49dceec53462c1002a
Andy Langer <mightydodongo@gmail.com>
2023-02-28 19:17:04 -0600

    Maybe this?

--- src/main.cpp
+++ src/main.cpp
@@ -2,7 +2,7 @@
 #include "CollisionMap.h"
 
 int main() {
-    std::cout << "HELLO WORLD!" << std::endl; 
+    std::cout << "HELLO WORLD 2!" << std::endl; 
     cm::CollisionMap collisionMap;
 
     collisionMap.makeSureItWorks();
